3f50bb5dd89ceeac784b4424cdda9027326b7b34
srchen <srchen@montreal.stanford.edu>
2023-10-12 18:30:23 -0700

    detected numerical issue in solving optimal transformation

--- isaacgymenvs/tests/poly_match.py
+++ isaacgymenvs/tests/poly_match.py
@@ -1,10 +1,5 @@
 import torch
 
-# S1 and S2 are set of paired points
-# Find optimal transformation from S1 to S2
-# TODO: Accelerate it and write it in batch form
-# TODO: Compute constraint violation
-# S1, S2, weight should be small, computation is sparse
 def optimal_transformation(S1, S2, weight):
     c1 = (weight.diag() @ S1).mean(dim=0)
     c2 = (weight.diag() @ S2).mean(dim=0)
@@ -48,14 +43,15 @@ def force_eq_reward(tip_pose, target_pose, compliance, friction_mu, current_norm
     R,t = optimal_transformation_batch(tip_pose, target_pose, compliance) # t: [num_envs, 3]
     tip_pose_eq = (R@tip_pose.transpose(1,2)).transpose(1,2) + t.unsqueeze(1)
     diff_vec = tip_pose_eq - target_pose # [num_envs, num_fingers, 3]
-    dir_vec = diff_vec / diff_vec.norm(dim=-1).unsqueeze(-1)
+    dir_vec = diff_vec / (diff_vec.norm(dim=-1).unsqueeze(-1) + 1e-6)
     # Rotate local norm to final
     normal_eq = (R @ current_normal.transpose(1,2)).transpose(1,2)
     # measure cos between
+    print(R)
     ang_diff =  torch.einsum("ijk,ijk->ij",dir_vec, normal_eq)
-    margin = (ang_diff - torch.sqrt(1/(1+torch.tensor(friction_mu)**2))).clamp(min=-0.99)
+    margin = (ang_diff - torch.sqrt(1/(1+torch.tensor(friction_mu)**2))).clamp(-0.99)
     # we hope margin to be as large as possible, never below zero
-    return torch.log(margin+1).sum(dim=-1)
+    return torch.log(margin+2.1).sum(dim=-1)
 
 def force_eq_check(tip_pose, target_pose, compliance, friction_mu, current_normal):
     """
@@ -84,15 +80,19 @@ def force_eq_check(tip_pose, target_pose, compliance, friction_mu, current_norma
 
 def verify(S1, S2, weight, R, t):
     S1 = (R @ S1.transpose(0,1)).transpose(0,1) + t
+    print(S1)
+    c = S1.mean(dim=0)
     F = weight.diag() @ (S2 - S1)
-    return F.sum(dim=0)
+    T = torch.cross(S1 - c.unsqueeze(0),F,dim=1)
+    return F.sum(dim=0),T.sum(dim=0)
 
 
 if __name__ == "__main__":
     dim = 3
-    A = torch.rand(1,dim,3,requires_grad=True)
-    B = torch.rand(1,dim,3)
-    w = torch.rand(1,dim)
-    r = force_eq_check(A, B, w, 0.5, A)
-    print(r)
+    A = torch.tensor([[[-0.05, 0.0, 0.03],[0.05,-0.04, 0.03],[0.05,0.0, 0.03],[0.05, 0.04, 0.03]]],requires_grad=True)
+    B = torch.tensor([[[-0.03,0., 0.03],[0.03,-0.04, 0.03],[0.03,0., 0.03],[0.03,0.04, 0.03]]])
+    N = torch.tensor([[[-1.,0., 0.0],[1.,0., 0.],[1.,0., 0.],[1.,0., 0.]]])
+    w = torch.tensor([[1.,1.,1.,1.]]) # Should be normalzied
+    R,t = optimal_transformation_batch(A,B,w)
+    print(verify(A.squeeze(), B.squeeze(), w.squeeze(), R.squeeze(), t.squeeze()))
 
