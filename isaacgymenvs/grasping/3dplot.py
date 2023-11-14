fix numerical issue and test partial pointcloud

isaacgymenvs/grasping/3dplot.py
@@ -50,9 +50,9 @@ for i in range(0, num_steps, args.stride):
     m = cm.ScalarMappable(norm=norm, cmap='jet')
     m.set_array([])
     fcolors = m.to_rgba(color_dimension)
-
+    contour_mask = (Z >-0.001) * (Z < 0.001)
+    fcolors[contour_mask] = [1,1,1,1]
     # Plot the surface.
-    print(fcolors)
     surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,facecolors=fcolors,
                         linewidth=0, antialiased=True)
     surf2 = ax.plot_surface(X, Y, np.zeros_like(Z))

isaacgymenvs/grasping/gpis.py
@@ -9,6 +9,8 @@ class GPIS:
             self.kernel_function = self.thin_plate_spline
         elif kernel == "rbf":
             self.kernel_function = self.exponentiated_quadratic
+        elif kernel == "joint":
+            self.kernel_function = self.joint_kernel
 
     def exponentiated_quadratic(self, xa, xb):
         # L2 distance (Squared Euclidian)
@@ -21,12 +23,15 @@ class GPIS:
             R = self.R
         sq_norm = torch.cdist(xa,xb)
         return 2 * sq_norm**3 - 3 * R * sq_norm**2 + R**3
+    
+    def joint_kernel(self, xa, xb):
+        return 0.3 * self.exponentiated_quadratic(xa, xb) + 0.7 * self.thin_plate_spline(xa, xb)
         
 
     # noise can also be a vector
     def fit(self, X1, y1, noise=0.0):
         # Find maximum pair wise distance within training data
-        if self.kernel_function == self.thin_plate_spline:
+        if self.kernel_function == self.thin_plate_spline or self.kernel_function == self.joint_kernel:
             self.R = torch.max(torch.cdist(X1, X1))
         self.X1 = X1
         self.y1 = y1 - self.bias
@@ -107,7 +112,7 @@ class GPIS:
     def get_visualization_data(self, lb, ub, steps=100):
         test_X = torch.stack(torch.meshgrid(torch.linspace(lb[0],ub[0],steps),
                                             torch.linspace(lb[1],ub[1],steps),
-                                            torch.linspace(lb[2],ub[2],steps),indexing="xy"),dim=3).to(self.X1.device) # [steps, steps, steps, 3]
+                                            torch.linspace(lb[2],ub[2],steps),indexing="xy"),dim=3).double().to(self.X1.device) # [steps, steps, steps, 3]
         test_mean, test_var = torch.zeros(steps,steps,steps), torch.zeros(steps,steps,steps)
         for i in range(steps):
             mean, var = self.pred2(test_X[i].view(-1,3)) # [steps**3]
@@ -129,8 +134,8 @@ if __name__ == "__main__":
     pcd = mesh.sample_points_poisson_disk(128)
     pcd.colors = o3d.utility.Vector3dVector(np.tile(np.asarray([0,1,0]), (len(pcd.points),1)))
     o3d.visualization.draw_geometries([pcd])
-    points = torch.from_numpy(np.asarray(pcd.points)).cuda().float()
-    weights = torch.rand(20,128).cuda().float()
+    points = torch.from_numpy(np.asarray(pcd.points)).cuda().double()
+    weights = torch.rand(20,128).cuda().double()
     # normalize dimension 1
     weights = torch.softmax(weights * 50, dim=1)
     print(weights.sum(dim=1))
@@ -142,13 +147,19 @@ if __name__ == "__main__":
     o3d.visualization.draw_geometries([pcd, new_pcd])
     gpis = GPIS(0.08, 1)
     externel_points = torch.tensor([[-0.1, -0.1, -0.1], [0.1, -0.1, -0.1], [-0.1, 0.1, -0.1],[0.1, 0.1, -0.1],
-                                    [-0.1, -0.1, 0.1], [0.1, -0.1, 0.1], [-0.1, 0.1, 0.1],[0.1, 0.1, 0.1]]).cuda()
-    y = torch.vstack([0.1 * torch.ones_like(externel_points[:,0]).cuda().view(-1,1),torch.zeros_like(points[:,0]).cuda().view(-1,1), 
+                                    [-0.1, -0.1, 0.1], [0.1, -0.1, 0.1], [-0.1, 0.1, 0.1],[0.1, 0.1, 0.1],
+                                    [-0.1,0., 0.], [0., -0.1, 0.], [0.1, 0., 0.], [0., 0.1, 0],
+                                    [0., 0., 0.1], [0., 0., -0.1]]).double().cuda()
+    
+    # Create mask for partial observed pointcloud
+    mask = points[:,0] > -1.0
+
+    y = torch.vstack([0.1 * torch.ones_like(externel_points[:,0]).cuda().view(-1,1),torch.zeros_like(points[mask][:,0]).cuda().view(-1,1), 
                       -0.1 * torch.ones_like(internal_points[:,0]).cuda().view(-1,1)])
     print(y)
-    gpis.fit(torch.vstack([externel_points, points, internal_points]), y,noise=torch.tensor([0.3] * len(externel_points)+
-                                                                                            [0.01] * len(points) + 
-                                                                                            [0.03] * len(internal_points)).cuda())
+    gpis.fit(torch.vstack([externel_points, points[mask], internal_points]), y,noise=torch.tensor([0.3] * len(externel_points)+
+                                                                                            [0.001] * len(points[mask]) + 
+                                                                                            [0.03] * len(internal_points)).double().cuda())
     test_mean, test_var, lb, ub = gpis.get_visualization_data([-0.1,-0.1,-0.1],[0.1,0.1,0.1],steps=100)
     np.savez("gpis.npz", mean=test_mean, var=test_var, ub=ub, lb=lb)
     plt.imshow(test_mean[:,:,50], cmap="seismic", vmax=0.1, vmin=-0.1)